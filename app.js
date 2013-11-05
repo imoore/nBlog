198dbe420ac1f2ed81bc8f2a5a55ba4605be0428
Ian Moore <imoore@4ud.co.uk>
2013-11-05 15:11:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -30,31 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-/*
-app.get('/',  function(req, res){
-    index.findAll(function(error, docs){
-        res.render('index',{
-          title: '',
-          links:docs
-        });
-    })
-});
-*/
+
 app.get('/', index.getHome);
 app.get('/:page', index.findByName);
-app.get('/backend/admin', function(req, res) { res.render('admin', { title: 'My Blog Backend' }) });
-//Testing post feture
-/*
-app.post('/backend/admin/signin', function(req, res) {
 
-    console.log(req.body.user + "<-Posted Data Test");
 
-    res.send('200');
-
-});
-*/
-app.post('/backend/admin/signin', admin.findByUser);
-app.get('/backend/index', function(req, res){ res.send('200') });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
